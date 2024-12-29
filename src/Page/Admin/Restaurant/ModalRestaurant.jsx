8d2476fbf8360e6f0a85eafcbc30d0242cf7fbef
quan0506 <quanphamm0506@gmail.com>
2024-12-30 02:19:44 +0700

    fix modal room,rest

--- src/Page/Admin/Restaurant/ModalRestaurant.jsx
+++ src/Page/Admin/Restaurant/ModalRestaurant.jsx
@@ -70,111 +70,101 @@ const ModalRestaurant = ({ type, data, isModalVisible, onClose, onSave, branches
     };
 
     return (
-      <div className="modal-admin">
-          <Modal
-            className="custom-modal"
+        <Modal
             title={type === "add" ? "Thêm Nhà hàng" : type === "edit" ? "Chỉnh sửa nhà hàng" : "View restaurant"}
             open={isModalVisible}
             onCancel={onClose}
             footer={
                 type === "view" ? (
-                  <Button onClick={onClose}>Đóng</Button>
+                    <Button onClick={onClose}>Đóng</Button>
                 ) : (
-                  <>
-                      <Button type="primary" onClick={handleSave}>
-                          Lưu
-                      </Button>
-                      <Button onClick={onClose}>Thoát</Button>
-                  </>
+                    <>
+                        <Button type="primary" onClick={handleSave}>
+                            Lưu
+                        </Button>
+                        <Button onClick={onClose}>Thoát</Button>
+                    </>
                 )
             }
-          >
-              <div className="grid grid-cols-2 gap-4">
-                  <div>
-                      <strong className="block text-sm font-medium text-gray-700 ">Chi Nhánh</strong>
-                      <Select
-                        value={form.branchId || ""}
+        >
+            <label>
+                <strong>Chi Nhánh</strong>
+                <Select
+                    value={form.branchId || ""}
 
-                        onChange={handleBranchChange}
-                        placeholder="Select Branch"
-                        style={{width: '100%', marginBottom: 16}}
-                      >
-                          {branches.map((branch) => (
-                            <Select.Option key={branch.id} value={branch.id}>
-                                {branch.branchName}
-                            </Select.Option>
-                          ))}
-                      </Select>
-                  </div>
+                    onChange={handleBranchChange}
+                    placeholder="Select Branch"
+                    style={{width: '100%', marginBottom: 16}}
+                >
+                    {branches.map((branch) => (
+                        <Select.Option key={branch.id} value={branch.id}>
+                            {branch.branchName}
+                        </Select.Option>
+                    ))}
+                </Select>
+            </label>
 
-                  <div>
-                      <strong className="block text-sm font-medium text-gray-700 mb-1">Loại Nhà Hàng</strong>
-                      <Input
-                        value={form.restaurantType || ""}
-                        onChange={(e) => setForm({...form, restaurantType: e.target.value})}
-                        placeholder="Enter Restaurant Type"
-                        style={{marginBottom: 16}}
-                      />
-                  </div>
-                  <div>
-                      <strong className="block text-sm font-medium text-gray-700 mb-1">Thời gian</strong>
-                      <Input
-                        value={form.time || ""}
-                        onChange={(e) => setForm({...form, time: e.target.value})}
-                        placeholder="Enter Time"
-                        style={{marginBottom: 16}}
-                      />
-                  </div>
-                  <div>
-                      <strong className="block text-sm font-medium text-gray-700 mb-1">Giá cho Người lớn</strong>
-                      <Input
-                        value={form.restaurantAdultPrice || ""}
-                        onChange={(e) => setForm({...form, restaurantAdultPrice: e.target.value})}
-                        placeholder="Enter Adult Price"
-                        style={{marginBottom: 16}}
-                      />
-                  </div>
-                  <div>
-                      <strong className="block text-sm font-medium text-gray-700 mb-1">Giá cho Trẻ em</strong>
-                      <Input
-                        value={form.restaurantChildrenPrice || ""}
-                        onChange={(e) => setForm({...form, restaurantChildrenPrice: e.target.value})}
-                        placeholder="Enter Children Price"
-                        style={{marginBottom: 16}}
-                      />
-                  </div>
-                  <div className="col-span-2">
-                      <strong className="block text-sm font-medium text-gray-700 mb-1">Mô tả</strong>
-                      <Input.TextArea
-                        value={form.carDescription || ""}
-                        onChange={(e) => setForm({...form, carDescription: e.target.value})}
-                        placeholder="Nhập Mô tả Xe"
-                        className="custom-textarea"
-                        rows={4}
-                      />
-                  </div>
+            <label>
+                <strong>Loại Nhà Hàng</strong>
+                <Input
+                    value={form.restaurantType || ""}
+                    onChange={(e) => setForm({...form, restaurantType: e.target.value})}
+                    placeholder="Enter Restaurant Type"
+                    style={{marginBottom: 16}}
+                />
+            </label>
+            <label>
+                <strong>Thời gian</strong>
+                <Input
+                    value={form.time || ""}
+                    onChange={(e) => setForm({...form, time: e.target.value})}
+                    placeholder="Enter Time"
+                    style={{marginBottom: 16}}
+                />
+            </label>
+            <label>
+                <strong>Giá cho Người lớn</strong>
+                <Input
+                    value={form.restaurantAdultPrice || ""}
+                    onChange={(e) => setForm({...form, restaurantAdultPrice: e.target.value})}
+                    placeholder="Enter Adult Price"
+                    style={{marginBottom: 16}}
+                />
+            </label>
+            <label>
+                <strong>Giá cho Trẻ em</strong>
+                <Input
+                    value={form.restaurantChildrenPrice || ""}
+                    onChange={(e) => setForm({...form, restaurantChildrenPrice: e.target.value})}
+                    placeholder="Enter Children Price"
+                    style={{marginBottom: 16}}
+                />
+            </label>
+            <label>
+                <strong>Mô tả</strong>
+                <Input
+                    value={form.restaurantDescription || ""}
+                    onChange={(e) => setForm({...form, restaurantDescription: e.target.value})}
+                    placeholder="Enter Restaurant Description"
+                    style={{marginBottom: 16}}
+                />
+            </label>
 
-                  <div className="col-span-2">
-                      <label className="block text-sm font-medium text-gray-700 mb-1">Tải lên hình ảnh:</label>
-                      <Upload
-                        listType="picture-card"
-                        fileList={fileList}
-                        onPreview={handlePreview}
-                        onChange={handleChange}
-                        beforeUpload={() => false}
-                        className="custom-upload"
-                      >
-                          <div>
-                              <UploadOutlined/>
-                              <div style={{marginTop: 8}}>Tải lên</div>
-                          </div>
-                      </Upload>
-                  </div>
-              </div>
-
-          </Modal>
-      </div>
-
+            <label>
+                <strong>Tải lên hình ảnh:</strong>
+                <Upload
+                    listType="picture"
+                    beforeUpload={(file) => {
+                        return false;
+                    }}
+                    onChange={handleChange}
+                    fileList={fileList}
+                    onPreview={handlePreview}
+                >
+                    <Button icon={<UploadOutlined/>}>Tải lên</Button>
+                </Upload>
+            </label>
+        </Modal>
     );
 };
 
